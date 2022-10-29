782f79c32d7581a0bd67f023483513e1ff20dfca
Stella <stellamaia97@hotmail.com>
2022-10-29 19:01:59 -0300

    Modificações Página de Login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="wrapper-background">
+    <div class="page-login-center">
+    <b-card img-top style="max-width: 20rem; " class="mb-2 page-card-home ">
+      <b-form class="container">
+        <h1 class="login-page-title">Entrar</h1>
+        <div>
+          <b-row>
+            <b-form-input v-model="email" class="form-login-name" type="email" placeholder="E-mail" required>
+            </b-form-input>
+          </b-row>
+          <b-row>
+            <b-form-input v-model="password" class="form-login-password" type="password" placeholder="Senha" required>
+            </b-form-input>
+          </b-row>
+          <button @click="login" class="login-page-botton"> Entrar</button>
+
+          <p class="register-account">Não tem uma conta ainda?<router-link class="link-register-account" to="/register"> <span class="register-link"> Cadastre-se</span> </router-link>
+          </p>
+        </div>
+      </b-form>
+
+    </b-card>
+  </div>
+
+  </div>
+</template>
+
+
+<script>
+import firebase from "firebase"
+
+export default {
+  name: 'LoginView',
+  data: () => {
+    return {
+      email: '',
+      password: '',
+      initials: ""
+
+    };
+  },
+  methods: {
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          (result) => {
+            localStorage.setItem("token", result.user.uid);
+            this.$router.push("/home");
+          },
+          () => {
+            this.$swal(
+              "Oops...",
+              "Você ainda não criou uma conta ou senha incorreta",
+              "error"
+            )
+          }
+        )
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.wrapper-background {
+  background-image:  url("../assets/black.jpg");
+  height: 100vh;
+  width: 100%;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+}
+.page-login-center{
+  position: relative!important;
+  top: 100px;
+}
+.page-card-home {
+  background-color: rgba(0, 0, 0, .75);
+  border-radius: 4px;
+  padding: 20px 10px 20px 10px;
+  position: relative;
+    display: inline-flex;
+}
+.login-page-title {
+  color: white;
+  padding-bottom: 10px;
+}
+.form-login-name,
+.form-login-password {
+  margin-bottom: 30px;
+}
+
+.login-page-botton {
+  border: none;
+  height: 40px;
+  width: 120px;
+  border-radius: 3px;
+  margin: 20px 0 30px 0;
+}
+.register-account {
+  color: white;
+  font-size: 12px;
+}
+
+.link-register-account {
+  color: rgb(157 157 157);
+  font-weight: bold;
+}
+
+.link-register-account:hover {
+  color: rgb(92, 92, 92);
+}
+.register-link{
+  font-size: 10px;
+}
+</style>
+
